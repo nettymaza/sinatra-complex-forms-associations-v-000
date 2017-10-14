7aabbe3396069c58816a43143568022473501172
Annette Maza <nettymaza@gmail.com>
2017-10-14 00:22:21 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -27,7 +27,7 @@ class PetsController < ApplicationController
   post '/pets/:id' do
     @pet = Pet.find(parms[:id])
     @pet.update(params["pet"])
-    if ! params["owner"]["name"].empty?
+    if !params["owner"]["name"].empty?
       @pet.owner = Owner.create(name: params["owner"]["name"])
     end
     @pet.save

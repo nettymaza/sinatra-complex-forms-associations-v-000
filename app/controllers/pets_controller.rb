93e3f2cf0596af5a155cb6e2308b64cc0e804158
Annette Maza <nettymaza@gmail.com>
2017-10-14 00:43:51 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -20,11 +20,6 @@ class PetsController < ApplicationController
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id/edit' do
-    @pet = Pet.find(params[:id])
-    erb :'/pets/edit'
-  end
-
   get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
@@ -37,8 +32,13 @@ class PetsController < ApplicationController
     if !params["owner"]["name"].empty?
       @pet.owner = Owner.create(name: params["owner"]["name"])
     end
-
     @pet.save
     redirect to "pets/#{@pet.id}"
   end
+
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
+  end
+
 end
